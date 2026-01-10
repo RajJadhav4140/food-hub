5fbf2476a13550d9ff68a48025e095ea6912d9f5
RajJadhav4140 <rajjadhav4140@gmail.com>
2026-01-10 14:01:43 +0530

    design food item page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import StoreContextprovider from "./context/StoreContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App /> 
-  </BrowserRouter> 
-)
+    <StoreContextprovider>
+      <App />
+    </StoreContextprovider>
+  </BrowserRouter>
+);
